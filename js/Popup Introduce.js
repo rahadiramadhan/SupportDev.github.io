def76db6176cafab6837954e8eadf54b395cbf62
Rahadi Ramadhan <ramadhanfattahyahya@gmail.com>
2022-11-24 00:02:33 +0800

    added .gitignore and remove bin/ from staging

---
+++ js/Popup Introduce.js
@@ -0,0 +1,10 @@
+alert("Welcome My Developer Support Ramadhan Fattah Yahya ");
+alert("Welcome My Developer Support Ramadhan Nakama Yahya Ch")
+
+if(alert + alert)
+{
+    console.log("Working");
+} else (alert + alert)
+{
+    console.log("not working");
+}
---
+++ js/Switch color Page.js
@@ -0,0 +1,7 @@
+const color = true;
+color = rgb;
+color = rgba;
+color = hsl;
+color = hex;
+
+
